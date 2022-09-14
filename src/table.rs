8922b6b17bf928fbe2bd1316ea2980cce64f9d0c
Anish Sinha <anishsinha0128@gmail.com>
2022-09-14 19:59:28 -0400

    Finish initial page table api.

--- src/table.rs
+++ src/table.rs
@@ -8,8 +8,21 @@ lazy_static! {
     pub static ref GLOBAL_PAGE_TABLE: PageTable = Arc::new(RwLock::new(HashMap::new()));
 }
 
-pub fn get(key: usize) {}
+pub fn get(key: usize) -> Option<usize> {
+    if let Ok(table) = GLOBAL_PAGE_TABLE.read() {
+        return Some((*table.get(&key).clone()));
+    }
+    None
+}
 
-pub fn set() {}
+pub fn set(key: usize, val: usize) {
+    if let Ok(mut table) = GLOBAL_PAGE_TABLE.write() {
+        table.insert(key, val);
+    }
+}
 
-pub fn remove() {}
+pub fn remove(key: usize) {
+    if let Ok(mut table) = GLOBAL_PAGE_TABLE.write() {
+        table.remove(&key);
+    }
+}
