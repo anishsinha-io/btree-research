231f6da2274c40b9df3ef407b9aa214a7c73b169
Anish Sinha <anishsinha0128@gmail.com>
2022-09-14 17:22:27 -0400

    Fix error in static reference to page table.

--- src/table.rs
+++ src/table.rs
@@ -5,5 +5,5 @@ use lazy_static::lazy_static;
 type PageTable = Arc<RwLock<HashMap<usize, usize>>>;
 
 lazy_static! {
-    pub static ref GLOBAL_PAGE_TABLE: PageTable = Arc::new(RwLock::new(HashMap::new::<usize, usize>()))
+    pub static ref GLOBAL_PAGE_TABLE: PageTable = Arc::new(RwLock::new(HashMap::new()));
 }
